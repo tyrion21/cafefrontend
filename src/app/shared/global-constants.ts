5f6aff49c8fd5ca1f291f479a35c2f133c376c6a
tyrion21 <jhermidadiaz@gmail.com>
2024-07-08 23:37:10 -0400

    Initial commit

---
+++ src/app/shared/global-constants.ts
@@ -0,0 +1,15 @@
+export class GlobalConstants{
+
+    //Message
+    public static genericError: string = "Something went wrong. Please try again later.";
+
+    //Regex
+    public static nameRegex: string = "[a-zA-Z0-9 ]*";
+
+    public static emailRegex: string = "^[a-z0-9._%+-]+@[a-z0-9._%-]+\\.[a-z]{2,3}";
+
+    public static phoneRegex: string = "^[e0-9]{10,10}$";
+
+    //Variable
+    public static error:string = "error";
+}
